035b76e82d2969b9bee760d79cdd1d9695318e22
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2022-11-29 15:30:44 +0200

    edit style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
 import Main from "./components/main";
-// import Boxes from "./components/boxes"
 function App() {
   // end the loop 
   return (
