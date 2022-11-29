5509d58e4aacc2423a50bb36e0901404502e7fef
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2022-11-29 13:52:37 +0200

    edit responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header>
             <div className="container">
-                <img src="images/Troll Face.png" alt="memeImg"></img>
+                <img src="images/Troll_Face.png" alt="memeImg" />
                 <h1>Meme Generator</h1>
             </div>
         </header>
